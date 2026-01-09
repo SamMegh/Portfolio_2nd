ed94da032017e8b842c68238f4a4237a13138a85
sam.megh0305@gmail.com <sam.megh0305@gmail.com>
2026-01-09 21:21:22 +0530

    blog data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import BlogSection from "./components/blogSection";
 import BottomBar from "./components/bottomBar"
 import SplashCursor from "./components/cursor"
 import UpperSection from "./components/upperSection"
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="h-screen flex flex-col">
       <UpperSection />
+      <BlogSection/>
       <BottomBar className="mt-auto" />
     </div>
   );
---
+++ src/components/blogSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BlogSection() {
+  return (
+    <div>
+      <h2>
+        BLOGS
+      </h2>
+    </div>
+  )
+}
+
+export default BlogSection
